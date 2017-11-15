9a4cc17ad649c7ed9255216b2232b3740f6f9639
yevgeniykrymov <yevgeniy.krymov@gmail.com>
2017-11-15 16:49:00 +0200

    15.11.2017

--- THEBAND/THEBAND/js/controls.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    //Simple filter controls
-    $('.custom-simplefilter li').click(function() {
-        $('.custom-simplefilter li').removeClass('active');
-        $(this).addClass('active');
-    });
-    
-});
